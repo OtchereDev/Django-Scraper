3154c92febd0786aecfce8c71dab2c26f98a7bf9
Oliver Otchere <Oliverotchere4@gmail.com>
2022-01-09 15:00:56 +0000

    added static setting

--- scrapper_api/settings.py
+++ scrapper_api/settings.py
@@ -116,3 +116,14 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+STATICFILES_DIRS = [
+    BASE_DIR/'static'
+]
+
+STATIC_ROOT = BASE_DIR/'static_root'
+
+MEDIA_URL='media/'
+
+MEDIA_ROOT= BASE_DIR/'media'
+
